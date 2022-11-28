8d9ce15803517ea8a635e71300a6c90d66ba1c15
Cuni <ahet1017@gmail.com>
2022-11-28 14:47:10 +0800

    Swiper

--- resources/views/Login/index.blade.php
+++ resources/views/Login/index.blade.php
@@ -14,8 +14,8 @@
     @section('content')
     <form action="{{ route('Login.store')}}" method="POST">
         @csrf
-        <div class="Account-Box STU-Register">
-            <div class="Title">
+        <div class="Account-Box">
+            <div class=" Title">
                 <h1>登入</h1>
             </div>
             <div class="Input-Section">
@@ -25,6 +25,7 @@
                     <a href="{{route('Signup.index')}}">註冊</a>
                     <a href="forgetPW.php">忘記密碼?</a>
                 </div>
+                <div style="height: 5em"></div>
             </div>
             <div class="Submit-Section">
                 <input class="Submit-Button" type="submit" value="提交" />
