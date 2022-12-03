4dfff3c139d32c4c241d757ffdcbc758bde346ca
Cuni <ahet1017@gmail.com>
2022-12-03 19:35:00 +0800

    STU_Chat

--- resources/views/IN/Student/Chat/index.blade.php
+++ resources/views/IN/Student/Chat/index.blade.php
@@ -14,19 +14,22 @@
     @section('content')
     @parent
     <div id="container">
-        <div id="Response-Box">
-            <a href="{{route("Chat.create")}}">新增評論</a>
-            <ul>
+        <div class="Response-Box">
+            <h1 class="text-center">意見反映</h1>
+            <ul class="Responses">
                 @foreach($Chats as $Chat)
-                <li>
-                    <div class="author">作者：{{$Chat->chat_maker}}</div>
-                    <div class="article">主旨：{{$Chat->chat_subject}}</div>
-                    <div class="time">時間：{{$Chat->chat_date}}</div>
-                    <hr>
-                    <div class="message">{{$Chat->chat_content}}</div>
+                <li class="Response-Card">
+                    <div class="Message-Detail">
+                        <div class="article">主旨：{{$Chat->chat_subject}}</div>
+                        <div class="author">作者：{{$Chat->chat_maker}}</div>
+                        <div class="time">時間：{{$Chat->chat_date}}</div>
+                    </div>
+                    <!-- <hr> -->
+                    <div class="Message">{{$Chat->chat_content}}</div>
                 </li>
                 @endforeach
             </ul>
+            <button type="button" class="btn btn-primary"><a class="text-white text-decoration-none" href="{{route("Chat.create")}}">新增評論</a></button>
             {{ $Chats->links()}}
         </div>
     </div>
