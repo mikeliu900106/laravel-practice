230740144f45dc8b973ec39bdd5772520c37c74c
Cuni <ahet1017@gmail.com>
2022-12-05 19:12:01 +0800

    STU_Experience

--- resources/views/IN/Student/Experience/update.blade.php
+++ resources/views/IN/Student/Experience/update.blade.php
@@ -2,33 +2,35 @@
 <html>
 @extends('layout.app')
 @section('head')
-    @parent
-    
+@parent
+
 @endsection
 
 <body>
-    @section('nav') 
-        @parent
+    @section('nav')
+    @parent
     @endsection
 
     @section('content')
-        @parent
-        {{ $user_id}}
-        <form action="{{route('Experience.update',$user_id)}}" method="post" enctype="multipart/form-data">
-            @csrf
-            @method("patch")
-            <br>
-            <span>心得上傳</span>
-            <input type="file" class="form-control" name="files"  />
-            <br>
-            <button type="submit" class="btn btn-dark">提交</button>
-        </form>
+    @parent
+    <div id="container">
+        <div class="File-Box">
+            <h1>心得</h1>
+            <div class="File-Upload">
+                <form action="{{route('Experience.update',$user_id)}}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    @method("patch")
+                    <input type="file" class="form-control" name="files" />
+                    <button type="submit" class="btn btn-dark">提交</button>
+                </form>
+            </div>
+        </div>
+    </div>
     @endsection
 
-
     @section('footer')
-        @parent
-    
+    @parent
+
     @endsection
 </body>
 
