66e80aec1989a5ec739c2987d21be58e3b2758aa
mikeliu900106 <mikeliu20010106@gmail.com>
2022-12-05 19:35:09 +0800

    company_chat

--- resources/views/IN/Company/Chat/index.blade.php
+++ resources/views/IN/Company/Chat/index.blade.php
@@ -2,65 +2,39 @@
 <html>
 @extends('layout.app')
 @section('head')
-    @parent
-    
+@parent
+
 @endsection
 
 <body>
-    @section('nav') 
-        @parent
+    @section('nav')
+    @parent
     @endsection
 
     @section('content')
-        @parent
-        <div id="wrap">
-            
-                <div id="responseBox">
-                    <ul>
-                        @foreach($Chats as $Chat)
-                            <li>
-                                <div class="author">作者：{{$Chat->chat_maker}}</div>
-                                <div class="article">主旨：{{$Chat->chat_subject}}</div>
-                                <div class="time">時間：{{$Chat->chat_date}}</div>
-                                <hr>
-                                <div class="message">{{$Chat->chat_content}}</div>
-                            </li>
-                        @endforeach
-
-                    </ul>
-    
-                    <?php if($Chat_level !='2') {?>
-                        <form class="leavecomment" action="{{route("CompanyChat.store")}}" method="post">
-                            @csrf
-                            <div class="author">
-                                <p>作者</p><input name="maker" type="text">
-                            </div>
-                            <div class="gist">
-                                <p>主旨</p><input name="subject" type="text">
-                                <br>
-                            </div>
-                            <div class="content">
-                                <p>內容</p><textarea name="content" id="" cols="30" rows="10"></textarea>
-                                <input type="submit" value="送出">
-                            </div>
-                        </form>
-                        <?php
-                        }else{
-                            echo"<div>你已被封禁沒資格發言</div>";    
-                        }?>
+    <div id="container">
+        <div class="Response-Box">
+            <h1 class="text-center">意見反映</h1>
+            <ul class="Responses">
+                @foreach($Chats as $Chat)
+                <li class="Response-Card">
+                    <div class="Message-Detail">
+                        <div class="article">主旨：{{$Chat->chat_subject}}</div>
+                        <div class="author">作者：{{$Chat->chat_maker}}</div>
+                        <div class="time">時間：{{$Chat->chat_date}}</div>
                     </div>
-                    {{ $Chats->links() }}
-            </div> 
-           
-            
-
+                    <!-- <hr> -->
+                    <div class="Message">{{$Chat->chat_content}}</div>
+                </li>
+                @endforeach
+            </ul>
+            <button type="button" class="btn btn-primary"><a class="text-white text-decoration-none" href="{{route("CompanyChat.create")}}">新增評論</a></button>
+            {{ $Chats->links() }}
+        </div>
+    </div>
     @endsection
 
-
-
     @section('footer')
-        @parent
-        
-    @endsection 
-  
+    @parent
+    @endsection
 </body>
