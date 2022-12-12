3fb9003b4ccfdb8adbfc61213a25f6a2f4fcb278
mikeliu900106 <mikeliu20010106@gmail.com>
2022-12-12 16:34:16 +0800

    廠商註冊

--- resources/views/IN/Company/register.blade.php
+++ resources/views/IN/Company/register.blade.php
@@ -12,8 +12,6 @@
     @endsection
 
     @section('content')
-    {{$random }}
-    {{$company_id}}
     <form method="post" action="{{route('Company.destroy',$company_id)}}">
         @method("delete")
         @csrf
@@ -25,6 +23,16 @@
                 <div>
                     <input type="text" name="input_random" class="Account_text" placeholder="書入字串">
                     <input type="hidden" name="random" value = "{{$random}}">
+                    <input type="hidden" name="company_id" value = "{{$company_datas["company_id"]}}">
+                    <input type="hidden" name="company_name" value = "{{$company_datas["company_name"]}}">
+                    <input type="hidden" name="company_title" value = "{{$company_datas["company_title"]}}">
+                    <input type="hidden" name="company_number" value = "{{$company_datas["company_number"]}}">
+                    <input type="hidden" name="county" value = "{{$company_datas["county"]}}">
+                    <input type="hidden" name="district" value = "{{$company_datas["district"]}}">
+                    <input type="hidden" name="address" value = "{{$company_datas["address"]}}">
+                    <input type="hidden" name="company_email" value = "{{$company_datas["company_email"]}}">
+                    <input type="hidden" name="company_username" value = "{{$company_datas["company_username"]}}">
+                    <input type="hidden" name="company_password" value = "{{$company_datas["company_password"]}}">
                     <label for="teacher_username">請輸入email內的字串</label>
                 </div>
             </div>
