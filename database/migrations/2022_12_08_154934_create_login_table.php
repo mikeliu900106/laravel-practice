eda4c6b5589a7a2ddbd446aeeb46579957d2114b
mikeliu900106 <mikeliu20010106@gmail.com>
2022-12-15 15:32:56 +0800

    login migrate

--- database/migrations/2022_12_08_154934_create_login_table.php
+++ database/migrations/2022_12_08_154934_create_login_table.php
@@ -16,7 +16,7 @@ class CreateLoginTable extends Migration
         Schema::create('loginBase', function (Blueprint $table) {
             $table->string('id',25)->comment("id");
             $table->string('username',30)->comment("帳號名稱");
-            $table->string('company_name',35)->comment("密碼名稱");
+            $table->string('password',35)->comment("密碼名稱");
             $table->string('level',3)->comment("等級");
             
         });
