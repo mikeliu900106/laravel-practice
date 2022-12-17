1c5eb9ae675a2c55464430ea4df255c0c30c86c4
Cuni <ahet1017@gmail.com>
2022-12-17 15:51:59 +0800

    m

--- app/Http/Controllers/PhpExcelController.php
+++ app/Http/Controllers/PhpExcelController.php
@@ -26,81 +26,79 @@ class PhpExcelController extends Controller
     {
         if ($request->session()->has('user_id')) {
             if ($request->session()->get('level') == '2') {
-              
+
                 $user_id = session()->get('user_id');
 
-                function getskill($skill_count,$skill,$date = 0){        
-                    for($i = 0; $i < count($skill);$i++){
+                function getskill($skill_count, $skill, $date = 0)
+                {
+                    for ($i = 0; $i < count($skill); $i++) {
                         $this_year = date('Y');
                         $top_year = $this_year - $date;
-                        $top_date = $top_year."-"."12"."-"."31";
-                        $last_date = $top_year."-"."1"."-"."1";
+                        $top_date = $top_year . "-" . "12" . "-" . "31";
+                        $last_date = $top_year . "-" . "1" . "-" . "1";
                         // echo $top_date;
                         // echo $last_date;
 
-                        $count = HistoryVacancies::where('vacancies_match','已有配對')
-                        ->where('vacancies_Skill','LIKE',"%{$skill[$i]}%")
-                        ->where('delete_time','<',$top_date)
-                        ->where('delete_time','>',$last_date)
-                        ->count();
-                        echo $count ;
-                        array_push($skill_count,$count);
-                        
+                        $count = HistoryVacancies::where('vacancies_match', '已有配對')
+                            ->where('vacancies_Skill', 'LIKE', "%{$skill[$i]}%")
+                            ->where('delete_time', '<', $top_date)
+                            ->where('delete_time', '>', $last_date)
+                            ->count();
+                        echo $count;
+                        array_push($skill_count, $count);
                     }
                     return $skill_count;
                 }
-                function getpair($date = 0){        
-                        $this_year = date('Y');
-                        $top_year = $this_year - $date;
-                        $top_date = $top_year."-"."12"."-"."31";
-                        $last_date = $top_year."-"."1"."-"."1";
+                function getpair($date = 0)
+                {
+                    $this_year = date('Y');
+                    $top_year = $this_year - $date;
+                    $top_date = $top_year . "-" . "12" . "-" . "31";
+                    $last_date = $top_year . "-" . "1" . "-" . "1";
 
-                        $pair_count = HistoryPair::
-                        where('teacher_confirm','已有配對')
-                        ->where('delete_time','<',$top_date)
-                        ->where('delete_time','>',$last_date)
+                    $pair_count = HistoryPair::where('teacher_confirm', '已有配對')
+                        ->where('delete_time', '<', $top_date)
+                        ->where('delete_time', '>', $last_date)
                         ->count();
-                        echo$pair_count;
+                    echo $pair_count;
                     return $pair_count;
                 }
                 if ($request->has('choose_date')) {
                     $date = $request->choose_date;
-                    $skill = array('React','Javascript','Vue','Angular','Mysql','SqlServer','Php','Laravel','c#','c++','java','python','kotlin');
+                    $skill = array('React', 'Javascript', 'Vue', 'Angular', 'Mysql', 'SqlServer', 'Php', 'Laravel', 'c#', 'c++', 'java', 'python', 'kotlin');
                     $skill_count = [];
-                    $skill_count_data = getskill($skill_count,$skill,$date);
+                    $skill_count_data = getskill($skill_count, $skill, $date);
                     $pair_count_data = getpair($date);
-                    $skill_count_data= json_encode($skill_count_data); 
+                    $skill_count_data = json_encode($skill_count_data);
                     //1.取user配對情況
                     //2.取user履歷
                     //3.取usert成績單    
-                    return view("IN.Teacher.PhpExcel.index",
+                    return view(
+                        "IN.Teacher.PhpExcel.index",
                         [
-                            "skill_count"=>$skill_count_data,
-                            "pair_count_data"=>$pair_count_data,
+                            "skill_count" => $skill_count_data,
+                            "pair_count_data" => $pair_count_data,
                             'date'          => $date,
                         ]
                     );
-
                 } else {
-                    $skill = array('React','Javascript','Vue','Angular','Mysql','SqlServer','Php','Laravel','c#','c++','java','python','kotlin');
+                    $skill = array('React', 'Javascript', 'Vue', 'Angular', 'Mysql', 'SqlServer', 'Php', 'Laravel', 'c#', 'c++', 'java', 'python', 'kotlin');
                     $skill_count = [];
-                    $skill_count_data = getskill($skill_count,$skill,$date = 0);
+                    $skill_count_data = getskill($skill_count, $skill, $date = 0);
                     $pair_count_data = getpair($date = 0);
                     // echo $userDatas;
                     // $skill_count_data = json_decode($json,true);
-                    $skill_count_data= json_encode($skill_count_data); 
+                    $skill_count_data = json_encode($skill_count_data);
                     // echo $skill_count_data;
-                    return view("IN.Teacher.PhpExcel.index",
+                    return view(
+                        "IN.Teacher.PhpExcel.index",
                         [
-                            "skill_count"=>$skill_count_data,
-                            "pair_count_data"=>$pair_count_data,
+                            "skill_count" => $skill_count_data,
+                            "pair_count_data" => $pair_count_data,
                             'date'          => $date,
                         ]
                     );
                 }
-
-                
-
             } else {
                 echo "你不是教師";
                 //1. 顯示錯誤2.錯誤controller
@@ -110,7 +108,6 @@ class PhpExcelController extends Controller
         } else {
             echo "你沒登入";
         }
-      
     }
 
     /**
--- resources/views/IN/Teacher/PhpExcel/index.blade.php
+++ resources/views/IN/Teacher/PhpExcel/index.blade.php
@@ -13,38 +13,38 @@
 
     @section('content')
     @parent
-        <form action = "{{route("PhpExcel.index")}}" method = "get" >
-            <select name = "choose_date">
-                <option value="9">十年前</option>
-                <option value="8">九年前</option>
-                <option value="7">八年前</option>
-                <option value="6">七年前</option>
-                <option value="5">六年前</option>
-                <option value="4">五年前</option>
-                <option value="3">大前年</option>
-                <option value="2">前年</option>
-                <option value="1">去年</option>
-                <option value="0">今年</option>
-            </select>
-            <input type =submit value ="送出">
+    <form action="{{route("PhpExcel.index")}}" method="get">
+        <select name="choose_date">
+            <option value="9">十年前</option>
+            <option value="8">九年前</option>
+            <option value="7">八年前</option>
+            <option value="6">七年前</option>
+            <option value="5">六年前</option>
+            <option value="4">五年前</option>
+            <option value="3">大前年</option>
+            <option value="2">前年</option>
+            <option value="1">去年</option>
+            <option value="0">今年</option>
+        </select>
+        <input type=submit value="送出">
 
-            <p>廠商要求技能次數</p>
-                <p>react:{{ $skill_count[0] }}次</p>
-                <p>Javascript:{{ $skill_count[1] }}次</p>
-                <p>Vue:{{ $skill_count[2] }}次</p>
-                <p>Angular:{{ $skill_count[3] }}次</p>
-                <p>Mysql:{{ $skill_count[4] }}次</p>
-                <p>SqlServer:{{ $skill_count[5] }}次</p>
-                <p>Php:{{ $skill_count[6] }}次</p>
-                <p>Laravel:{{ $skill_count[7] }}次</p>
-                <p>c#:{{ $skill_count[8] }}次</p>
-                <p>c++:{{ $skill_count[9] }}次</p>
-                <p>java:{{ $skill_count[10] }}次</p>
-                <p>python:{{ $skill_count[11] }}次</p>
-                <p>kotlin:{{ $skill_count[12] }}次</p>
-            <p>學生配對成功次數</p>
-                <p>配對成功人數:{{$pair_count_data}}</p>
-        </form>
+        <p>廠商要求技能次數</p>
+        <p>react:{{ $skill_count[0] }}次</p>
+        <p>Javascript:{{ $skill_count[1] }}次</p>
+        <p>Vue:{{ $skill_count[2] }}次</p>
+        <p>Angular:{{ $skill_count[3] }}次</p>
+        <p>Mysql:{{ $skill_count[4] }}次</p>
+        <p>SqlServer:{{ $skill_count[5] }}次</p>
+        <p>Php:{{ $skill_count[6] }}次</p>
+        <p>Laravel:{{ $skill_count[7] }}次</p>
+        <p>c#:{{ $skill_count[8] }}次</p>
+        <p>c++:{{ $skill_count[9] }}次</p>
+        <p>java:{{ $skill_count[10] }}次</p>
+        <p>python:{{ $skill_count[11] }}次</p>
+        <p>kotlin:{{ $skill_count[12] }}次</p>
+        <p>學生配對成功次數</p>
+        <p>配對成功人數:{{$pair_count_data}}</p>
+    </form>
     @endsection
 
 
