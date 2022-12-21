98187e4c38b7b4f152fcaebd6368aff39bb15c63
mikeliu900106 <mikeliu20010106@gmail.com>
2022-12-21 22:55:03 +0800

    phpexcel vacancies

--- app/Http/Controllers/PhpExcelController.php
+++ app/Http/Controllers/PhpExcelController.php
@@ -70,7 +70,8 @@ class PhpExcelController extends Controller
                     $top_date = $top_year . "-" . "12" . "-" . "31";
                     $last_date = $top_year . "-" . "1" . "-" . "1";
 
-                    $vacancies_count = Vacancies::where('delete_time', '<', $top_date)
+                    $vacancies_count = Vacancies::where('teacher_watch','通過')
+                        ->where('delete_time', '<', $top_date)
                         ->where('delete_time', '>', $last_date)
                         ->count();
                     // echo $pair_count;
