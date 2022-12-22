04bf7f6697fa4ecaee8ce7b778525ea9146bbebf
Cuni <ahet1017@gmail.com>
2022-12-22 10:59:03 +0800

    m

--- public/js/index.js
+++ public/js/index.js
@@ -13,16 +13,16 @@ var __webpack_exports__ = {};
 // console.log(123)
 
 //Pair-Box
-var PB_tbody = document.querySelector("#Pair-Box-tbody");
-// console.log(PB_tbody.rows.length)
-if (PB_tbody.rows.length === 0) {
-  var tr = document.createElement("tr");
-  var td = document.createElement("td");
-  tr.append(td);
-  PB_tbody.append(tr);
-  td.colSpan = "100";
-  td.innerHTML = "暫無資料";
-  td.className = "text-center";
-}
+// let PB_tbody = document.querySelector("#Pair-Box-tbody");
+// // console.log(PB_tbody.rows.length)
+// if (PB_tbody.rows.length === 0) {
+//     let tr = document.createElement("tr");
+//     let td = document.createElement("td");
+//     tr.append(td);
+//     PB_tbody.append(tr);
+//     td.colSpan = "100";
+//     td.innerHTML = "暫無資料";
+//     td.className = "text-center";
+// }
 /******/ })()
 ;
--- resources/views/IN/Student/Pair/show.blade.php
+++ resources/views/IN/Student/Pair/show.blade.php
@@ -60,6 +60,19 @@
 
     @section('footer')
     @parent
+    <script>
+        let PB_tbody = document.querySelector("#Pair-Box-tbody");
+        // console.log(PB_tbody.rows.length)
+        if (PB_tbody.rows.length === 0) {
+            let tr = document.createElement("tr");
+            let td = document.createElement("td");
+            tr.append(td);
+            PB_tbody.append(tr);
+            td.colSpan = "100";
+            td.innerHTML = "暫無資料";
+            td.className = "text-center";
+        }
+    </script>
     @endsection
 </body>
 
